32f823c70d546ca2c5574c86234ab4fdd9bf4a0b
Muhammad Haris Javed <m.harisjaved487@gmail.com>
2022-06-14 03:11:52 +0500

    added developer folio theme front page

---
+++ precache-manifest.f433f914fd3abc008d5db8ea38013062.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5697418fb38d67c58d88a8f17dbf7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79712b3058206fc2907",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "9a1a6e212e46b93378b4",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "dbc78e8e81ff7b6ecf7c",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "44b748fb40cf4f31b111",
+    "url": "/static/js/2.0398d4b4.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.0398d4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79712b3058206fc2907",
+    "url": "/static/js/3.5037e296.chunk.js"
+  },
+  {
+    "revision": "9a1a6e212e46b93378b4",
+    "url": "/static/js/4.aa2ec175.chunk.js"
+  },
+  {
+    "revision": "dbc78e8e81ff7b6ecf7c",
+    "url": "/static/js/main.21bb2b6e.chunk.js"
+  },
+  {
+    "revision": "75d03172df7eade8cf5c",
+    "url": "/static/js/runtime-main.8717dc6e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "90f9e6b355f728f4de62110d71494c75",
+    "url": "/static/media/SWVL_logo.90f9e6b3.bin"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ad12d2af0f61b3a100428bde4637a0eb",
+    "url": "/static/media/flight_simulator_game.ad12d2af.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "f93095f88bfca138ffd642365e1a9c42",
+    "url": "/static/media/remotebase_logo.f93095f8.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "f2c5379bb2541a95b737461289c4b31a",
+    "url": "/static/media/university_of_karachi_logo.f2c5379b.webp"
+  }
+]);
